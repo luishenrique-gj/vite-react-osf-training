b9eb9615b2cfcf9171a3c489f4f2b70b5f0f11bf
luishenrique-gj <luis.juca@osf.digital>
2025-01-08 14:38:24 -0300

    feat: test environment setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,37 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    parser: '@babel/eslint-parser',
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      'plugin:react/recommended'
+    ],
+    files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true
+      },
       globals: globals.browser,
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'react': react,
+    },
+    settings: {
+      react: {
+        version: 'detect', // Automatically detect the react version
+      },
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +39,7 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'react/prop-types': 'off', // Example rule, adjust as needed
     },
-  },
-)
+  }
+);
