00e8e56a3090ba9c41e2456407b60f8fcbffaf48
luishenrique-gj <luis.juca@osf.digital>
2025-01-13 15:46:38 -0300

    fix: updated tests, updated project dependencies

---
+++ src/jest.setup.js
@@ -0,0 +1,2 @@
+
+global.TextEncoder = require('util').TextEncoder;
--- src/test/SideMenu.test.tsx
+++ src/test/SideMenu.test.tsx
@@ -1,34 +1,30 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { menuOptions } from '../constants/menuOptions';
 import SideMenu from '../components/sideMenu/SideMenu';
+import {  StaticRouter } from 'react-router-dom';
 
 describe('SideMenu Component', () => {
 
-    const mockHandleAction = jest.fn();
   it('renders text correctly', () => {
-    render(<SideMenu handleSelectedOption={mockHandleAction} selectedOptionKey=''/>);
+    render(
+        <StaticRouter location='/'>
+            <SideMenu />
+        </StaticRouter>
+    );
 
     expect(screen.getByText('Available Challenges')).toBeInTheDocument();
     expect(screen.getByText('select a challenge')).toBeInTheDocument();
   });
 
   it('renders all available options', () => {
-    render(<SideMenu handleSelectedOption={mockHandleAction} selectedOptionKey='' />);
+    render(<StaticRouter location='/'>
+                <SideMenu />
+            </StaticRouter>
+    );
 
     menuOptions.forEach(option => {
       expect(screen.getByText(option.name)).toBeInTheDocument();
-    });
-  });
-
-  it('calls handleSelectedOption with the correct option when an option is clicked', () => {
-    const mockHandleSelectedOption = jest.fn();
-    render(<SideMenu handleSelectedOption={mockHandleSelectedOption} selectedOptionKey='' />);
-
-    menuOptions.forEach(option => {
-      const optionElement = screen.getByText(option.name);
-      fireEvent.click(optionElement);
-      expect(mockHandleSelectedOption).toHaveBeenCalledWith(option.key);
     });
   });
 });
