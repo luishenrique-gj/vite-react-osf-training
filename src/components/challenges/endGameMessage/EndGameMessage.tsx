c21e32a6c7d4f99e197d169917965cf2d8189cba
luishenrique-gj <luis.juca@osf.digital>
2025-01-13 15:18:46 -0300

    feat:updated endgame message component

--- src/components/challenges/endGameMessage/EndGameMessage.tsx
+++ src/components/challenges/endGameMessage/EndGameMessage.tsx
@@ -1,16 +1,34 @@
+import { useMemo } from "react";
 import "./EndGameMessage.css";
 
 
 interface EndgameMessageProps {
-    case: number | null;
+    playerChoice: number | null;
+    opponentChoice:number | null;
 }
 
-const EndgameMessage = ({ case: gameCase }: EndgameMessageProps) => {
+const EndgameMessage = ({ playerChoice: playerChoice, opponentChoice:opponentChoice}: EndgameMessageProps) => {
+
+    const endGameCase = useMemo(() => {
+        const winningConditions = [[0, 2], [1, 0], [2, 1]];
+        const isPlayerWinner = winningConditions.some(
+            ([player, opponent]) => player === playerChoice && opponent === opponentChoice
+        );
+
+        if(playerChoice === null || opponentChoice === null) {
+            return null;
+        }
+    
+        if (playerChoice === opponentChoice) {
+            return "It's a draw!";
+        }
+    
+        return isPlayerWinner ? 'You won!' : 'You lost!';
+    }, [playerChoice, opponentChoice]);
+
     return ( 
         <div className="EndgameMessage">
-            {gameCase === 1 && <p>It's a draw!</p>}
-            {gameCase === 2 && <p>You won!</p>}
-            {gameCase === 3 && <p>You lost!</p>}
+                {endGameCase}
         </div> 
     );
 }
