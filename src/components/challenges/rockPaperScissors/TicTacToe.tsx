85feb83aa43c4852b44cbda39d65fee33ffbba8a
luishenrique-gj <luis.juca@osf.digital>
2025-01-13 15:16:23 -0300

    feat:implemented placeholder challenge

---
+++ src/components/challenges/rockPaperScissors/TicTacToe.tsx
@@ -0,0 +1,19 @@
+import "./RockPaperScissors.scss"
+
+const TicTacToe = () => {
+    return ( 
+        <div className="RockPaperScissors">
+                <h2>
+                   Tic Tac Toe
+                </h2>
+                <div className="RockPaperScissors__game-board">
+                    <div>
+                        <h1>TEST COMPONENT</h1>
+                    </div>
+                </div>
+        </div>
+
+     );
+}
+ 
+export default TicTacToe;
