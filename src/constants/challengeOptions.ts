9c35edd487c576a870d20f5e069bce502aa322e3
luishenrique-gj <luis.juca@osf.digital>
2025-01-13 15:17:56 -0300

    feat:added new route for placeholder component

--- src/constants/challengeOptions.ts
+++ src/constants/challengeOptions.ts
@@ -1,6 +1,8 @@
 import RockPaperScissors from "../components/challenges/rockPaperScissors/RockPaperScissors";
+import TicTacToe from "../components/challenges/rockPaperScissors/TicTacToe";
 import { menuKeys } from "./menuOptions";
 
 export const challengeList = [
-    { key: menuKeys.RockPaperScissors, component: RockPaperScissors },
+    { key: menuKeys.RockPaperScissors, component: RockPaperScissors, path: "/rock-paper-scissors" },
+    {key: "tic-tac-toe", component:TicTacToe , path: "/tic-tac-toe"}
 ];
--- src/constants/menuOptions.ts
+++ src/constants/menuOptions.ts
@@ -3,12 +3,19 @@ export const menuOptions = [
     {
         name: "Rock Paper Scissors",
         key: "rock-paper-scissors", 
-    }
+        path: "/rock-paper-scissors"
+    },
+    {
+        name: "Tic Tac Toe",
+        key: "tic-tac-toe",
+        path: "/tic-tac-toe"
+    },
 ]
 
 export interface IMenuOptions {
     name: string;
     key: string;
+    path:string,
 }
 
 
